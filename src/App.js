a999fcc408c4692b2dfb6596c35972524fda509c
DavidHuertasF <david.huertasf@gmail.com>
2022-10-12 18:01:31 -0500

    implementación useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import {useState} from "react"
 import simpsonsApi from "./APIrest/simponsApi";
 import Card from "./Card";
 import "./App.css";
 
 function App() {
+
+  const [filter, setFilter] = useState("");
+
   return (
     <div className="principal_container">
       <h1>Characters</h1>
+      <input id="filter" name="filter" type="text" value={filter}
+        onChange={(event)=> setFilter(event.target.value)}
+      />
       <div className='cards-container'>
-        {simpsonsApi.map((character, i) => (
+        {simpsonsApi.filter(personaje => personaje.character.includes(filter)).map((character, i) => (
           <Card
             key={i}
             name={character.character}
