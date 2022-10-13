e5b9834ffbe38bd84e31efe392008411764ae2d8
DavidHuertasF <david.huertasf@gmail.com>
2022-10-12 20:26:52 -0500

    add real api simpsons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import {useState} from "react"
+import { useState, useEffect } from "react";
 import simpsonsApi from "./APIrest/simponsApi";
 import Card from "./Card";
 import "./App.css";
 
 function App() {
-
   const [filter, setFilter] = useState("");
+  const [characters, setCharacters] = useState([]);
+
+  const getImages = async () => {
+    const characters = await simpsonsApi();
+    setCharacters(characters);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
 
   return (
     <div className="principal_container">
       <h1>Characters</h1>
-      <input id="filter" name="filter" type="text" value={filter}
-        onChange={(event)=> setFilter(event.target.value)}
+      <input
+        id="filter"
+        name="filter"
+        type="text"
+        value={filter}
+        onChange={(event) => setFilter(event.target.value)}
       />
-      <div className='cards-container'>
-        {simpsonsApi.filter(personaje => personaje.character.includes(filter)).map((character, i) => (
-          <Card
-            key={i}
-            name={character.character}
-            image={character.image}
-            quote={character.quote}
-          />
-        ))}
+
+      <div className="cards-container">
+        {characters
+          .filter((personaje) => personaje.nombre.includes(filter))
+          .map((character, i) => (
+            <Card
+              key={i}
+              name={character.nombre}
+              image={character.image}
+              quote={character.frase}
+            />
+          ))}
       </div>
     </div>
   );
